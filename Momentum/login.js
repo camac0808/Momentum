6477f4f24cab80fb199d9d6fefa3e6c652659cda
camac0808 <85935958+camac0808@users.noreply.github.com>
2022-02-28 00:31:29 +0900

    Update login.js

--- Momentum/login.js
+++ Momentum/login.js
@@ -2,12 +2,14 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const savedUsername = localStorage.getItem("username");
+const logoutForm = document.querySelector("#logout-form");
 
 if(savedUsername === null){
   loginForm.classList.remove("hidden");
   loginForm.addEventListener("submit", onLoginSubmit);
 } else{
   loginForm.classList.add("hidden");
+  logoutForm.classList.remove("hidden");
   paintGreetings(savedUsername);
 }
 
@@ -18,9 +20,20 @@ function paintGreetings(name){
 
 function onLoginSubmit(event){
   event.preventDefault();
-  loginForm.classList.add("hidden")
+  loginForm.classList.add("hidden");
+  logoutForm.classList.remove("hidden");
   const username = loginInput.value;
   localStorage.setItem("username", username);
   paintGreetings(username);
 }
 
+function onLogoutSubmit(event){
+  event.preventDefault();
+  logoutForm.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+  greeting.classList.add("hidden");
+  localStorage.removeItem("username");
+  loginInput.value = "";
+}
+
+logoutForm.addEventListener("submit", onLogoutSubmit);
