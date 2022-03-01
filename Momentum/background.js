44f44d226c58c8365e56dec520af4312cf9286cd
Hyunsik Choi <gomgom_@naver.com>
2022-03-01 21:54:42 +0900

    배경수정
    배경수정

--- Momentum/background.js
+++ Momentum/background.js
@@ -1,4 +1,5 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"] ;
+const images = ["0.jpeg", "1.jpeg", "2.jpeg","2164D13E57DC1048117254.jpg", "2368B3405353F7881FBE59-1.jpg", "2454CF3D5353F793104715.jpg"] ;
+
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 const bgImage = document.createElement("img");
